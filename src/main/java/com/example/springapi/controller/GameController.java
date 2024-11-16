88f0afb5444f471d5ea2fab20eca62aa68af68c2
Philemon <pshcl44556@gmail.com>
2024-11-16 16:48:52 +0100

    Updates

--- src/main/java/com/example/springapi/controller/GameController.java
+++ src/main/java/com/example/springapi/controller/GameController.java
@@ -121,7 +121,7 @@ public class GameController {
     // First two cards are for Blue, second two are for Red, last one is the
     // additional card
     @PostMapping("/createGameKonrad")
-    public ResponseEntity<String> createGameKonrad(@RequestParam String[] cards) {
+    public ResponseEntity<String> createGameKonrad(@RequestParam("cards") String[] cards) {
         String gameId = gameService.createKonradGame(cards);
         return ResponseEntity.ok(gameId);
 
